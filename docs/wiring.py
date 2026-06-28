7f38ffb6fbf0a97fe8fd969fc4b8260d9b33cae0
Martin Hagelin <martin@hagelin.nu>
2026-06-28 13:11:51 +0200

    action fix 2

--- docs/wiring.py
+++ docs/wiring.py
@@ -4,11 +4,11 @@ import schemdraw.elements as elm
 
 outfile = Path(__file__).with_suffix(".svg")
 
-with schemdraw.Drawing(file=str(outfile)) as d:
-    d += elm.SourceV().label("5V")
-    d += elm.Resistor().label("220Ω")
-    d += elm.LED()
-    d += elm.Ground()
+d = schemdraw.Drawing()
 
-# IMPORTANT: just save, do NOT open
+d += elm.SourceV().label("5V")
+d += elm.Resistor().label("220Ω")
+d += elm.LED()
+d += elm.Ground()
+
 d.save(str(outfile))
